fc4c441045b7f010ed7c4a59d02f9f4d06a0bf24
unknown <benja.k.h@gmail.com>
2020-07-29 08:46:11 -0600

    now with smartlights

---
+++ statebutton/libraries.h
@@ -0,0 +1,36 @@
+#include <Adafruit_BME280.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_SSD1306.h>
+#include <colors.h>
+#include <Encoder.h>
+#include <Ethernet.h>
+#include <hue.h>
+#include <mac.h>
+#include <OneButton.h>
+#include <SPI.h>
+#include <SD.h>
+#include <wemo.h> 
+#include <Wire.h>
+
+IPAddress ip(192,168,1,16);
+Adafruit_NeoPixel pixel(12, 16, NEO_GRB + NEO_KHZ800);
+Encoder myEnc(14, 15);
+OneButton button1(23, true, true);
+
+int wemo[] = {0, 1, 2 ,3};
+int Z = 0;
+int p = 0;
+int pinOut = 16;
+int buttonPin = 23;
+int colorArrayHue[] = {HueRed, HueOrange, HueYellow, HueGreen, HueBlue, HueIndigo, HueViolet};
+int colorArray[] = {violet, blue, purple, cyan, teal, green, yellow, orange, maroon, red};
+int bulb[] = {0, 1, 2, 3, 4};
+bool status = true;
+
+int posW, old_posW, pos2W, newValW, j, lastbright, lastbulb, lastx, bright, pos, newVal, x, y;
+bool laststateW, buttonStateW, stateW, buttonstate, laststate, hueState, activated;
+float humidRH, pressPA, tempC, tempF, hpa, inHg;
+
+byte i;
+byte count = 0;
--- statebutton/statebutton.ino
+++ statebutton/statebutton.ino
@@ -1,20 +1,31 @@
-#include "OneButton.h"
+#include "libraries.h"
+
 OneButton GreenButton(21, false, true);
 
 const int green_led = 12;
 const int red_led = 4;
 int GreenButtonState[] = {0, 1, 2, 3};
-int G;
+int G = 1;
 //bool flash = false; 
 
 void setup() {
-initGreenButton();
-
+  initGreenButton();
+  initButtonLights();
+  initethernet();
+  initLightsSetup();
 }
 
 void loop() {
+  button1.tick();
   GreenButton.tick();
   Serial.printf("%i \n", G);
+
+  while (G== 0){
+    button1.tick();
+    GreenButton.tick();
+    Brightness();
+    BulbFunction();
+  }
 }
 void clickGB(){
   GreenButtonState[G];
@@ -32,4 +43,90 @@ void doubleclickGB(){
 void initGreenButton() {
   GreenButton.attachClick(clickGB);
   GreenButton.attachDoubleClick(doubleclickGB);
+}
+void click1Lights(){
+  Serial.println("Click");
+  hueState = !hueState;
+}
+
+void doubleClick1Lights(){
+  Serial.println("DoubleClick");
+  x++;
+  if (x > 7) {
+    x=0;
+    }
+}
+
+void longPress1Lights(){
+  y++;
+  if(y>5){
+    y=1;
+    }
+}
+
+void initButtonLights(){
+  button1.attachClick(click1Lights);
+  button1.attachDoubleClick(doubleClick1Lights);
+  button1.attachLongPressStop(longPress1Lights);
+  button1.setDebounceTicks(100);
+}
+
+void initethernet(){
+  Serial.begin(9600);
+  Ethernet.begin(mac,ip);
+  delay(2000);              // Wait for Serial Monitor
+  Serial.println("connecting...");
+  Serial.println(Ethernet.linkStatus());
+    // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  Serial.println();
+  Serial.print("LinkStatus: ");
+}
+
+void initLightsSetup() {
+  Serial.println(" Ready.");
+  x=2;
+  y=1;
+  setHue(bulb[y], true, colorArrayHue[x], bright);
+  buttonstate = true; 
+  laststate = false;
+  hueState = true;
+  lastx = x;
+  lastbright = bright;
+  lastbulb = y;
+}
+
+void Brightness() {
+    pos = myEnc.read();
+  if (pos > 255){
+    pos = 255;
+    myEnc.write(255);   
+      }
+  if (pos < 0){
+   pos = 0;
+    }
+  bright = pos;
+}
+
+void BulbFunction() {
+  char *a = "ON";
+  if((hueState!=laststate)||(bright != lastbright) || (x != lastx) || (y != lastbulb)){
+    setHue(bulb[y], hueState, colorArrayHue[x], bright);
+    laststate = hueState;
+    lastbright = bright;
+    lastx = x;
+    lastbulb = y;
+      if (hueState == true){
+       a = "ON";
+        }
+      else{
+        a = "OFF";
+          }
+    Serial.printf("Bulb %i is %s, Color Value = %i, Brightness %i\n",  y, a, colorArrayHue[x], bright);
+    } 
 }
